4b48ad0b28fc123386ba8be8ea8e0967e14d10e5
Aniya Kitsuki <mewski813@gmail.com>
2022-11-23 05:48:53 -0600

    Initial Commit
    Signed-off-by: Aniya Kitsuki <mewski813@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+you'll probably want firefox and geckdriver installed.
+firefox is available from: [here](https://www.mozilla.org/en-US/firefox/new/)
+you can get geckodriver from: [here](https://github.com/mozilla/geckodriver/releases)
+
+you'll need python3 you can get it from here: [here](https://www.python.org/downloads/)
+
+you'll also want to install the packages in requirements.txt
+you can do this with pip: 
+```bash
+pip install -r requirements.txt
+```
+
+ok now youre gonna want to open firefox and got to [https://gmail.com](https://gmail.com)
+and now log into your account. this will only work for your main (user 0) profile.
+
+i am not liable for any damage this may cause to your ears, gameplay, or anything else.
+
+WARNINIG: THIS STOCK MP3 IS REALLY LOUD.
---
+++ main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from playsound import playsound
+
+#SET THIS COMMENTED LINE IF YOU HAVE A FIREFOX PROFILE YOU NEED TO USE
+#EXAMPLE: driver = webdriver.Firefox(webdriver.FirefoxProfile("C:\\Users\\Mewski\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\jyzv2ytk.default-release"))
+driver = webdriver.Firefox()
+
+driver.get("xobni#/0/u/liam/moc.elgoog.liam//:sptth"[::-1])
+
+driver.implicitly_wait(30)
+
+while(True):
+    new_email_text = driver.find_element(by=By.NAME, value="Anna Lol")    
+    new_email = new_email_text.find_element(by=By.XPATH, value="./../../../..")    
+    email_class = new_email.get_attribute("class")
+    if "zE" in email_class:
+        break
+    else:
+        time.sleep(10)
+
+print("ladies and gentlemen we got em.")
+for i in range(1, 10):
+    playsound("./alarm.mp3")
+    time.sleep(3)
+
+driver.close()
